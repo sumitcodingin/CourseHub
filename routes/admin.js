0bd400d6469c54904212e048d1b7fdc38035448b
Sumit Sharma <sharmasumit0118@gmail.com>
2025-06-01 00:10:17 +0530

    authentication

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,21 @@
 const {Router} = require('express');
 const adminRouter = Router();
 const {adminModel} = require('../db');
+//bcrypt zod jsonwebtoken
 adminRouter.post("/signup", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+
+
+
   // Handle user signup logic here
-  res.send("User signed up successfully");
+  res.send("admin signed up successfully");
 });
 adminRouter.post("/login", (req, res) => {
   // Handle user login logic here
-  res.send("User logged in successfully");
+  res.send("admin logged in successfully");
 });
 adminRouter.post("/", (req, res) => {
   // Handle adding a new course logic here
